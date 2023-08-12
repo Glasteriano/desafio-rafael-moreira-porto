3c8e4c47de6354437ef4c7e029a6bf78a4231804
Rafael Pôrto <rafaelmportto@gmail.com>
2023-08-12 10:42:49 -0300

    :sparkles: funcionalidade: verificar carrinho zerado

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -4,10 +4,16 @@ class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
         const pagamentoValido = pagamentoAceito(metodoDePagamento);
+        //----------------------------------------------------------------
 
         if (!pagamentoValido) {
             return "Forma de pagamento inválida!";
         };
+
+        if (itens.length === 0) {
+            return "Não há itens no carrinho de compra!"
+        };
+        //----------------------------------------------------------------
 
         return "";
     }
